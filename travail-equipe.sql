06ba65c5b2bd3e7a7686655890a5a0393e536cf5
Xx-Nasank-xX <125129998+Xx-Nasank-xX@users.noreply.github.com>
2025-03-04 14:52:45 +0100

    Update travail-equipe.sql

--- travail-equipe.sql
+++ travail-equipe.sql
@@ -1,25 +1,26 @@
-DROP SCHEMA IF EXISTS analyse CASCADE;
-CREATE SCHEMA analyse;
+DROP SCHEMA IF EXISTS "analyse" CASCADE;
+CREATE SCHEMA "analyse";
 SET SCHEMA 'analyse';
 
-CREATE TABLE analyse.auteurs(
-  pid INT PRIMARY KEY
-  nom VARCHAR(255),
+CREATE TABLE auteurs(
+  pid INT PRIMARY KEY,
+  nom VARCHAR(255)
 );
 
-CREATE TABLE analyse.publications (
+CREATE TABLE publications (
     id INT PRIMARY KEY,
     score INT,
     titre VARCHAR(1000),
-    année INT,
-    type VARCHAR(200),
+    lieu VARCHAR(50),
+    annee INT,
+    acces VARCHAR(200),
     format VARCHAR(50),
     url VARCHAR(500)
 );
 
-CREATE TABLE analyse.publication_auteurs (
+CREATE TABLE publication_auteurs (
     publication_id INT,
-    auteur_pid VARCHAR(50),
+    auteur_pid INT,
     FOREIGN KEY (publication_id) REFERENCES publications(id),
     FOREIGN KEY (auteur_pid) REFERENCES auteurs(pid),
     PRIMARY KEY (publication_id, auteur_pid)
