f0525eed652a30be9f54eca4d1ff7f03de0e39d6
marius <icelos@yahoo.com>
2025-03-18 13:49:23 +0100

    bon url openalex

--- php/getvaluejson.php
+++ php/getvaluejson.php
@@ -23,7 +23,7 @@ require_once("db_connection.php");
 
         $encoded_doi = urlencode($doi);
         print_r($encoded_doi);
-        $url = "https://api.openalex.org/works/https://doi.org/" . $encoded_doi;
+        $url = "https://api.openalex.org/works/doi:" . $encoded_doi;
         print_r($url);  
         $response = file_get_contents($url);
     
