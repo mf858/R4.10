4aeed8669ef0ed3cff0109e4cbceeed21630cfa7
marius <icelos@yahoo.com>
2025-03-24 10:46:21 +0100

    suppr 2

--- php/tests.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mots_json = file_get_contents("../json/mots.json");
-$mots = json_decode($mots_json,true);
- 
-foreach($mots as $domain){
-    $liste_mots = $domain["keywords"];
-    print_r($liste_mots);
-} 
-?>
