1b1dc9f814f8daa474a55e819c4ec582632d9e8d
Xx-Nasank-xX <maelanpotier05@gmail.com>
2025-03-24 10:50:57 +0100

    derien les gg

--- php/db_connection.php
+++ php/db_connection.php
@@ -7,15 +7,12 @@ $password = 'Baptiste85'; // Mot de passe
 $port = 5432; // Port par défaut de PostgreSQL
 
 try {
-    // Création d'une instance PDO pour PostgreSQL
     $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $username, $password);
 
-    // Définir le mode d'erreur de PDO sur Exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     echo "Connexion réussie à PostgreSQL !";
 } catch (PDOException $e) {
-    // En cas d'erreur, afficher un message
     die("Erreur de connexion : " . $e->getMessage());
 }
 
