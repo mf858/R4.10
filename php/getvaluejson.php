35c9d7e2f835f46068a7d69df9371d5c54a1ddc2
Xx-Nasank-xX <maelanpotier05@gmail.com>
2025-03-24 11:09:27 +0100

    enlever les comments

--- php/getvaluejson.php
+++ php/getvaluejson.php
@@ -1,16 +1,15 @@
 <?php
-require_once("db_connection.php"); // Inclusion du fichier de connexion à PostgreSQL
+require_once("db_connection.php");
 
 // Charger le fichier JSON contenant les publications
 $file = file_get_contents("../json/publ.json");
 $decoded_data = json_decode($file, true);
 $data = $decoded_data["result"]["hits"]["hit"]; // Accès aux publications dans le JSON
 
-$pid_auteurs = []; // Tableau pour stocker les auteurs déjà insérés afin d'éviter les doublons
+$pid_auteurs = []; // Tableau pour stocker les auteurs 
 $j = 0; // Compteur pour les affiliations des auteurs
 
 for ($i = 0; $i < count($data); $i++) {
-    // Récupération des données principales de la publication
     $id = $data[$i]["@id"];
     $score = $data[$i]["@score"];
     $titre = $data[$i]["info"]["title"];
@@ -22,8 +21,6 @@ for ($i = 0; $i < count($data); $i++) {
     $auteurs = $data[$i]["info"]["authors"]["author"];
     $doi = $data[$i]["info"]["doi"];
 
-    print_r($doi); // Affichage du DOI pour debug
-
     // Recherche API OpenAlex pour récupérer des informations complémentaires
     $encoded_doi = urlencode($doi);
     print_r($encoded_doi);
@@ -31,26 +28,25 @@ for ($i = 0; $i < count($data); $i++) {
     $url = "https://api.openalex.org/works/doi:" . $encoded_doi;
     print_r($url);  
     
-    $response = @file_get_contents($url); // Utilisation de '@' pour éviter les erreurs fatales si l'API ne répond pas
-
-    $domaine = "Inconnu"; // Valeur par défaut si aucune donnée trouvée
+    $response = file_get_contents($url);
+    $domaine = "Inconnu"; // Valeur par défaut
     $authorship = [];
 
     if ($response !== FALSE) {
         $datalex = json_decode($response, true);
         
-        // Vérification si la clé "concepts" existe et récupération du premier concept comme domaine
+        //récupération du domaine
         if (isset($datalex['concepts']) && count($datalex['concepts']) > 0) {
             $domaine = $datalex["concepts"][0]["display_name"];
         }
 
-        // Vérification si la clé "authorships" existe et récupération des auteurs
+        //récupération des auteurs
         if (isset($datalex['authorships'])) {
             $authorship = $datalex["authorships"];
         }
     }
 
-    // Insertion des données de la publication dans PostgreSQL
+    // Insertion des données de la publication dans la base de données
     $stmt = $pdo->prepare("
         INSERT INTO groupes.publications(id, score, titre, lieu, annee, acces, format, url, domaine)
         VALUES(:id, :score, :titre, :lieu, :annee, :acces, :format, :url, :domaine)
